4e81fa72f5186cff4c8676611a1f761e6bca0c7f
NAGATA Hiroaki <nagata@handlena.me>
2011-07-30 10:54:47 +0900

    add auto-install settings

---
+++ .emacs.d/inits/30_auto-install.el
@@ -0,0 +1,5 @@
+;; http://www.emacswiki.org/emacs/download/auto-install.el
+(require 'auto-install)
+(auto-install-update-emacswiki-package-name t)
+(auto-install-compatibility-setup)
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)

ffd2a2f542730b20138052a3a862bbb67a7302cb
NAGATA Hiroaki <nagata@handlena.me>
2011-08-01 02:08:47 +0900

    popwin causes error using on emacs24

--- .emacs.d/inits/30_popwin.el
+++ .emacs.d/inits/30_popwin.el
@@ -1,11 +1,12 @@
 ;; INSTALL
-;; (install-elisp "https://github.com/m2ym/popwin-el/raw/v0.2/popwin.el")
-(if (require 'popwin nil t)
-    (progn
-      (setq display-buffer-function 'popwin:display-buffer)
-      (setq popwin:popup-window-height 0.3)
-      (setq anything-samewindow nil)
-      (push '("\\*anything-?" :regexp t) popwin:special-display-config)
-      (push '("\\*[Vv][Cc]" :regexp t) popwin:special-display-config)
-      (push '("\\*git-" :regexp t) popwin:special-display-config)
-      (push '("*image-dired*") popwin:special-display-config)))
+;; (install-elisp "https://raw.github.com/m2ym/popwin-el/master/popwin.el")
+(when (< emacs-major-version 24)
+  (if (require 'popwin nil t)
+      (progn
+        (setq display-buffer-function 'popwin:display-buffer)
+        (setq popwin:popup-window-height 0.3)
+        (setq anything-samewindow nil)
+        (push '("\\*anything-?" :regexp t) popwin:special-display-config)
+        (push '("\\*[Vv][Cc]" :regexp t) popwin:special-display-config)
+        (push '("\\*git-" :regexp t) popwin:special-display-config)
+        (push '("*image-dired*") popwin:special-display-config))))

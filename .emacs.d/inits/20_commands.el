5559a78acc6f80342b7876ac452d9df00ce82e74
NAGATA Hiroaki <nagata@handlena.me>
2011-08-01 16:41:10 +0900

    add custom commands

--- .emacs.d/inits/20_commands.el
+++ .emacs.d/inits/20_commands.el
@@ -3,3 +3,29 @@
   "Return face used at point."
   (interactive)
   (message "%s" (get-char-property (point) 'face)))
+
+;; kill all buffers
+(defun kill-all-buffers ()
+  (interactive)
+  (loop for buffer being the buffers
+        do (kill-buffer buffer)))
+
+;; for mobile phone templates
+(defun ktai-hankaku-katakana-region (start end)
+  (interactive "r")
+  (while (string-match
+          "[０-９Ａ-Ｚａ-ｚァ-ンー：；＄]+\\|[！？][！？]+"
+          (buffer-substring start end))
+    (save-excursion
+      (japanese-hankaku-region
+       (+ start (match-beginning 0))
+       (+ start (match-end 0))))))
+
+(defun ktai-hankaku-katakana-buffer ()
+  (interactive)
+  (ktai-hankaku-katakana-region (point-min) (point-max)))
+
+;; display string width in region
+(defun string-width-in-region (start end)
+  (interactive "r")
+  (princ (string-width (buffer-substring start end))))

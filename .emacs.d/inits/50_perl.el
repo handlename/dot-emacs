7b85f8777701a2d438b62d6b812d320c673f2dc0
NAGATA Hiroaki <nagata@handlena.me>
2011-08-04 00:35:50 +0900

    add flymake settings for cperl-mode

--- .emacs.d/inits/50_perl.el
+++ .emacs.d/inits/50_perl.el
@@ -106,12 +106,42 @@
   (interactive "r")
   (shell-command-on-region begin end "perl"))
 
+;; flymake
+;; http://unknownplace.org/memo/2007/12/21#e001
+(require 'flymake)
+(defvar flymake-perl-err-line-patterns
+  '(("\\(.*\\) at \\([^ \n]+\\) line \\([0-9]+\\)[,.\n]" 2 3 nil 1)))
+
+(defconst flymake-allowed-perl-file-name-masks
+  '(("\\.pl$" flymake-perl-init)
+    ("\\.pm$" flymake-perl-init)
+    ("\\.t$" flymake-perl-init)))
+
+(defun flymake-perl-init ()
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace))
+         (local-file (file-relative-name
+                      temp-file
+                      (file-name-directory buffer-file-name))))
+    (list "perl" (list "-wc" local-file))))
+
+(defun flymake-perl-load ()
+  (interactive)
+  (set-perl5lib)
+  (defadvice flymake-post-syntax-check
+    (before flymake-force-check-was-interrupted)
+    (setq flymake-check-was-interrupted t))
+  (ad-activate 'flymake-post-syntax-check)
+  (setq flymake-allowed-file-name-masks
+        (append flymake-allowed-file-name-masks flymake-allowed-perl-file-name-masks))
+  (setq flymake-err-line-patterns flymake-perl-err-line-patterns)
+  (flymake-mode t))
+
 ;; hook
 (add-hook
  'cperl-mode-hook
  '(lambda ()
     (progn
-      'flymake-perl-load
+      (flymake-perl-load)
       (local-set-key (kbd "C-x m") 'perldoc-m)
-      (local-set-key (kbd "C-x C-e") 'perl-eval)
-      )))
+      (local-set-key (kbd "C-x C-e") 'perl-eval))))

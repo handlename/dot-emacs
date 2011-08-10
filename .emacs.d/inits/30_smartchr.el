0e5a80b5895b522cce7c0d8dafca75d4903e27e3
NAGATA Hiroaki <nagata@handlena.me>
2011-08-10 15:15:53 +0900

    update smartchr settings

--- .emacs.d/inits/30_smartchr.el
+++ .emacs.d/inits/30_smartchr.el
@@ -5,11 +5,9 @@
 
 (defun my-smartchr-setting ()
   (local-set-key (kbd ">") (smartchr '(">" "->" ">>")))
-
-  (local-set-key (kbd "(") (smartchr '("(" "(`!!')")))
-  (local-set-key (kbd "{") (smartchr '("{" "{ `!!' }" "{\n`!!'\n}")))
-  (local-set-key (kbd "[") (smartchr '("[" "[`!!']")))
-  )
+  (local-set-key (kbd "(") (smartchr '("(" "(`!!')") "(("))
+  (local-set-key (kbd "{") (smartchr '("{" "{ `!!' }" "{\n`!!'\n}" "{{")))
+  (local-set-key (kbd "[") (smartchr '("[" "[`!!']" "[["))))
 
 (add-hook 'php-mode-hook 'my-smartchr-setting)
 (add-hook 'javascript-mode-hook 'my-smartchr-setting)

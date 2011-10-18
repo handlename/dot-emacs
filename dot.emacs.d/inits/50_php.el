37e0bad5b5be2b102adc71c5485919df9cb0c603
NAGATA Hiroaki <nagata@handlena.me>
2011-10-19 00:13:25 +0900

    add settings for php-mode

---
+++ dot.emacs.d/inits/50_php.el
@@ -0,0 +1,15 @@
+;; INSTALL
+;; (install-elisp-from-emacswiki "php-mode-improved.el")
+(autoload 'php-mode "php-mode")
+(require 'php-mode)
+(setq php-mode-force-pear t)
+(defun pear/php-mode-init()
+  "Set some buffer-local variables."
+  (setq case-fold-search t)
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'arglist-close 0))
+(add-hook 'php-mode-hook 'pear/php-mode-init)
+
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (c-set-style "stroustrup")))

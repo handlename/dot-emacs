30eb99120382d1634f3bc3e57ad647ccfb102d88
NAGATA Hiroaki <nagata@handlena.me>
2011-10-18 23:55:59 +0900

    update org-mode settings

--- .emacs.d/inits/50_org.el
+++ .emacs.d/inits/50_org.el
@@ -21,6 +21,7 @@
 (setq org-directory "~/note/")
 (setq org-my-private-file (concat org-directory "private.org"))
 (setq org-my-kayac-file (concat org-directory "kayac.org"))
+(setq org-my-dil-file (concat org-directory "dil.org"))
 
 (setq org-default-notes-file org-my-private-file)
 (setq org-agenda-files
@@ -36,9 +37,25 @@
         ("t" "Todo kayac" entry (file+olp org-my-kayac-file "Todo" "Future") "** TODO %?\n   %U\n   %i\n" :unnarrowed t)
         ("i" "Idea kayac" entry (file+headline org-my-kayac-file "Idea") "** %?\n   %U\n   %i\n" :unnarrowed t)
         ("m" "Memo kayac" entry (file+headline org-my-kayac-file "Memo") "** %?\n   %U\n   %i\n" :unnarrowed t)
+        ("d" "Do It Later" entry (file+headline org-my-dil-file "DIL") "** %?\n   %U\n   %i\n" :unnarrowed t)
         ))
 
 ;; todo
 (setq org-use-fast-todo-selection t)
 (setq org-todo-keywords
       '((sequence "TODO(t)" "STARTED(s)" "WAITING(w)" "|" "DONE(x)" "CANCEL(c)")))
+
+;; org-babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((C . t)
+   (css . t)
+   (emacs-lisp . t)
+   (js . t)
+   (latex . t)
+   (lisp . t)
+   (perl . t)
+   (python . t)
+   (ruby . t)
+   (sh . t)
+   (sql . t)))

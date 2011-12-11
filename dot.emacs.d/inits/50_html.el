79b9050f557d3c7438aa1a9bee9cf962b22dd413
NAGATA Hiroaki <nagata@handlena.me>
2011-12-11 22:04:47 +0900

    edit .tx and .ejs by html-helper-mode

--- dot.emacs.d/inits/50_html.el
+++ dot.emacs.d/inits/50_html.el
@@ -1,11 +1,16 @@
 ;; http://www.nongnu.org/baol-hth/
 ;; https://gist.github.com/672655
 (autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
+
 (setq auto-mode-alist (cons '("\\.html$" . html-helper-mode) auto-mode-alist))
-(setq html-helper-basic-offset 0)
+(setq auto-mode-alist (cons '("\\.ejs$" . html-helper-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.tx$" . html-helper-mode) auto-mode-alist))
+
+(setq html-helper-basic-offset 4)
 (setq html-helper-item-continue-indent 0)
 (setq html-helper-never-indent t)
 (setq html-helper-verbose nil)
+
 (defvar html-helper-new-buffer-template
   '("<!DOCTYPE html>\n"
     "<html lang=\"ja\">\n"

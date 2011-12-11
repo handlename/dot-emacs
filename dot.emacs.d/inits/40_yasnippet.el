946abb5e4462ccf2684667335cce898d87a755a0
NAGATA Hiroaki <nagata@handlena.me>
2011-12-11 22:02:40 +0900

    update face for yasnippet

--- dot.emacs.d/inits/40_yasnippet.el
+++ dot.emacs.d/inits/40_yasnippet.el
@@ -1,7 +1,11 @@
 ;; http://code.google.com/p/yasnippet/
 (require 'yasnippet)
+
 (yas/initialize)
 (setq yas/root-directory "~/.emacs.d/assets/snippets")
 (yas/load-directory yas/root-directory)
-(set-face-background 'yas/field-highlight-face "#404040")
 (global-set-key (kbd "C-c s") 'yas/insert-snippet)
+
+(set-face-background  'yas/field-highlight-face nil)
+(set-face-underline-p 'yas/field-highlight-face t)
+(set-face-underline   'yas/field-highlight-face "#404040")

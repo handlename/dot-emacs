84e5757e21937e4ffc82bb8be743a58676844612
NAGATA Hiroaki <nagata@handlena.me>
2012-02-28 11:44:26 +0900

    update settings for smartchr

--- dot.emacs.d/inits/30_smartchr.el
+++ dot.emacs.d/inits/30_smartchr.el
@@ -6,14 +6,25 @@
 (defun my-smartchr-setting ()
   (local-set-key (kbd ">") (smartchr '(">" "->" ">>")))
   (local-set-key (kbd "(") (smartchr '("(" "(`!!')" "((")))
-  (local-set-key (kbd "{") (smartchr '("{" "{ `!!' }" "{\n`!!'\n}" "{{")))
+  (local-set-key (kbd "{") (smartchr '("{" "{`!!'}" "{\n`!!'\n}" "{{")))
   (local-set-key (kbd "[") (smartchr '("[" "[`!!']" "[[")))
   (local-set-key (kbd "\"") (smartchr '("\"" "\"`!!'\"" "\"\"")))
   (local-set-key (kbd "'") (smartchr '("'" "'`!!''" "''"))))
 
-(dolist (mode '(cperl-mode-hook
+(dolist (mode '(c-mode-hook
+                cperl-mode-hook
+                emacs-lisp-mode-hook
                 javascript-mode-hook
                 js2-mode-hook
+                objc-mode-hook
                 php-mode-hook
                 ruby-mode-hook))
   (add-hook mode 'my-smartchr-setting))
+
+(add-hook 'objc-mode-hook
+          '(lambda ()
+             (progn
+               (local-set-key (kbd "@") (smartchr '("@" "@\"`!!'\"" "@@"))))))
+
+(add-hook 'cperl-mode-hook 'my-smartchr-setting)
+(print cperl-mode-hook)

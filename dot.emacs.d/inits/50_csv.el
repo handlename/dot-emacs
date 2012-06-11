fedefeee55485536e7b6180cac714994a46add17
NAGATA Hiroaki <nagata@handlena.me>
2012-06-11 10:48:10 +0900

    update settings for csv-mode

--- dot.emacs.d/inits/50_csv.el
+++ dot.emacs.d/inits/50_csv.el
@@ -4,5 +4,3 @@
 (add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
 (autoload 'csv-mode "csv-mode"
   "Major mode for editing comma-separated value files." t)
-
-(set-face-foreground 'csv-separator-face "#888888")

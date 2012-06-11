60a44a291385a0654bbe270a08dd21666147ed6e
NAGATA Hiroaki <nagata@handlena.me>
2012-06-11 11:10:50 +0900

    add imenu settings for objc-mode

--- dot.emacs.d/inits/50_objc.el
+++ dot.emacs.d/inits/50_objc.el
@@ -4,3 +4,16 @@
 
 ;; align
 (setq align-c++-modes (cons 'objc-mode align-c++-modes))
+
+;; imenu
+(defun objc-imenu-create-index ()
+  (let (index)
+    (dolist (pattern (list "\\(^@interface.+$\\|^@implementation.+$\\|#pragma mark.+$\\|^-.+$\\|^+.+$\\)" ))
+      (goto-char (point-min))
+      (while (re-search-forward pattern (point-max) t)
+        (push (cons (match-string 1) (match-beginning 1)) index)))
+    (nreverse index)))
+
+(add-hook 'objc-mode-hook
+          (lambda ()
+            (setq imenu-create-index-function 'objc-imenu-create-index)))

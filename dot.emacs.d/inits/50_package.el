02b11977c543584ba81782b41e39d6399fa59609
NAGATA Hiroaki <nagata@handlena.me>
2012-06-11 11:18:48 +0900

    add settings for package

---
+++ dot.emacs.d/inits/50_package.el
@@ -0,0 +1,8 @@
+;; INSTALL
+;; (install-elisp "http://repo.or.cz/w/emacs.git/blob_plain/1a0a666f941c99882093d7bd08ced15033bc3f0c:/lisp/emacs-lisp/package.el")
+
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa"))
+(setq package-user-dir (concat user-emacs-directory "vendor/elpa"))
+(package-initialize)

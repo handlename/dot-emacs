55788b34fca9ef37fe2795ebc15377c2609c41f6
NAGATA Hiroaki <nagata@handlena.me>
2012-06-11 11:09:13 +0900

    update settings for magit

--- dot.emacs.d/inits/50_magit.el
+++ dot.emacs.d/inits/50_magit.el
@@ -3,3 +3,4 @@
 (set-face-foreground 'magit-diff-add "#00ff00")
 (set-face-foreground 'magit-diff-del "#ff0000")
 (set-face-foreground 'magit-diff-hunk-header "#00ffff")
+(set-face-background 'magit-item-highlight nil)

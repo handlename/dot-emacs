eeb20235386a5e1b70503c393c4d5e5ed61fcba8
NAGATA Hiroaki <nagata@handlena.me>
2012-06-11 11:24:07 +0900

    use ace-jump-mode instead of searchchar

--- dot.emacs.d/inits/20_commands.el
+++ dot.emacs.d/inits/20_commands.el
@@ -30,50 +30,6 @@
   (interactive "r")
   (princ (string-width (buffer-substring start end))))
 
-;; search char
-;; http://dev.ariel-networks.com/wp/documents/aritcles/emacs/part16
-;; List3
-(defvar last-search-char nil)
-(defvar last-search-direction 'forward)
-
-(defun search-forward-with-char (char)
-  (interactive "cMove to Char: ")
-  (if (eq (char-after (point)) char) (forward-char))
-  (and (search-forward (char-to-string char) nil t)
-       (backward-char))
-  (setq last-search-char char
-        last-search-direction 'forward))
-
-(defun search-backward-with-char (char)
-  (interactive "cMove backward to Char: ")
-  (search-backward (char-to-string char) nil t)
-  (setq last-search-char char
-        last-search-direction 'backward))
-
-(defun search-repeat-with-char ()
-  (interactive)
-  (cond
-   ((eq nil last-search-char) (message "You haven't searched yet. Stupid!"))
-   ((eq last-search-direction 'forward)
-    (or (search-forward-with-char last-search-char) (backward-char)))
-   ((eq last-search-direction 'backward) (search-backward-with-char last-search-char))))
-
-(defun search-forward-with-char-sequential ()
-  (interactive)
-  (if (eq last-command this-command)
-      (search-repeat-with-char)
-    (search-forward-with-char (string-to-char (read-key-sequence "cMove to Char:")))))
-
-(defun search-backward-with-char-sequential ()
-  (interactive)
-  (if (eq last-command this-command)
-      (search-repeat-with-char)
-    (search-backward-with-char (string-to-char (read-key-sequence "cMove backward to Char:")))))
-
-(global-set-key (kbd "C--") 'search-forward-with-char-sequential)
-(global-set-key (kbd "C-$") 'search-backward-with-char-sequential)
-
-
 ;; expand and kill region if not select region
 ;; http://dev.ariel-networks.com/wp/documents/aritcles/emacs/part16
 ;; List9 mod
---
+++ dot.emacs.d/inits/40_ace-jump.el
@@ -0,0 +1,4 @@
+;; INSTALLED FROM package.el
+
+(require 'ace-jump-mode)
+(global-set-key (kbd "C--") 'ace-jump-mode)

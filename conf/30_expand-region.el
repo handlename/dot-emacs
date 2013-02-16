3d6835ae5c0aa6f9686129dc092d1687ee95ddc7
yabuki-ryosuke <yabuki-ryosuke@kayac.com>
2013-02-16 21:09:15 +0900

    キーバインドの変更

--- conf/30_expand-region.el
+++ conf/30_expand-region.el
@@ -1,2 +1,4 @@
 (require 'expand-region)
-(global-set-key (kbd "C-\'") 'er/expand-region)
+(global-set-key (kbd "M-1") 'mark-whole-buffer)
+(global-set-key (kbd "M-2") 'er/expand-region)
+(global-set-key (kbd "M-3") 'mark-defun)

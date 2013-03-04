c92a71b0df3cde670b76411c9ae57073a97dc08b
handlename <nagata@handlena.me>
2013-03-04 11:22:04 +0900

    use shosti/elscreen

--- dot.emacs.d/inits/30_elscreen.el
+++ dot.emacs.d/inits/30_elscreen.el
@@ -1,8 +1,11 @@
 ;; http://www.morishima.net/~naoto/software/elscreen/
-(load "elscreen" "ElScreen" t)
+(require 'elscreen)
+
 (setq elscreen-display-tab nil)
 (define-key global-map (kbd "M-t") 'elscreen-next)
 
 ;; hide number of current screen.
 (setq-default elscreen-e21-mode-line-string nil)
 (remove-hook 'elscreen-screen-update-hook 'elscreen-e21-mode-line-update)
+
+(elscreen-start)

d024a56c272e905e2a0d830571ef8fa0b26961bf
Konboi <ryosuke.yabuki@gmail.com>
2013-04-15 11:58:28 +0900

    Added command

--- conf/30_magit-mode.el
+++ conf/30_magit-mode.el
@@ -3,6 +3,7 @@
 (push '(magit-status :position left :height 0.5) popwin:special-display-config)
 
 (global-set-key (kbd "C-c git") 'magit-status)
+(global-set-key (kbd "C-c p") 'magit-push)
 (global-set-key (kbd "C-c dci") 'magit-svn-dcommit)
 
 

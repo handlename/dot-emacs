d2497541b0f38b644e230d70a8122fe45308401c
Konboi <ryosuke.yabuki@gmail.com>
2013-04-15 12:40:04 +0900

    Deleted command

--- conf/30_magit-mode.el
+++ conf/30_magit-mode.el
@@ -3,7 +3,6 @@
 (push '(magit-status :position left :height 0.5) popwin:special-display-config)
 
 (global-set-key (kbd "C-c git") 'magit-status)
-(global-set-key (kbd "C-c push") 'magit-push)
 (global-set-key (kbd "C-c dci") 'magit-svn-dcommit)
 
 

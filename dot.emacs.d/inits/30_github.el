005a3c747c51730bd46182917761e07be6b759b3
handlename <nagata@handlena.me>
2013-04-18 18:30:27 +0900

    add open-github

---
+++ dot.emacs.d/inits/30_github.el
@@ -0,0 +1,4 @@
+(require 'open-github)
+(global-set-key (kbd "C-c o f") 'open-github-from-file)
+(global-set-key (kbd "C-c o c") 'open-github-from-commit)
+(global-set-key (kbd "C-c o i") 'open-github-from-issues)
--- dot.emacs.d/inits/30_misc.el
+++ dot.emacs.d/inits/30_misc.el
@@ -100,30 +100,6 @@
             (if (zerop (call-process-shell-command "git rev-parse --show-toplevel"))
                 (git-gutter))))
 
-;; open-github
-;; http://shibayu36.hatenablog.com/entry/2013/01/18/211428
-(defun chomp (str)
-  (replace-regexp-in-string "[\n\r]+$" "" str))
-
-(defun git-project-p ()
-  (string=
-   (chomp
-    (shell-command-to-string "git rev-parse --is-inside-work-tree"))
-   "true"))
-
-(defun open-github-from-current ()
-  (interactive)
-  (cond ((and (git-project-p) (use-region-p))
-         (shell-command
-          (format "open-github-from-file %s %d %d"
-                  (file-name-nondirectory (buffer-file-name))
-                  (line-number-at-pos (region-beginning))
-                  (line-number-at-pos (region-end)))))
-        ((git-project-p)
-         (shell-command
-          (format "open-github-from-file %s %d"
-                  (file-name-nondirectory (buffer-file-name))
-                  (line-number-at-pos))))))
 
 ;; whitespace
 (setq whitespace-style

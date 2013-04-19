3ff00f2dcf73efa5638f6d87e414b28c9c3aa253
Konboi <ryosuke.yabuki@gmail.com>
2013-04-19 19:48:10 +0900

    Removed wrong key bind

--- init/30_mark-multiple.el
+++ init/30_mark-multiple.el
@@ -1,8 +1,9 @@
 ;; mark-multiple
-(require 'inline-string-rectangle)
-(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+;(require 'inline-string-rectangle)
+;(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
 (require 'mark-more-like-this)
+
 (global-set-key (kbd "C-,") 'mark-previous-like-this)
 (global-set-key (kbd "C-.") 'mark-next-like-this)
 ;; (global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)

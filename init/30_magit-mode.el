1a29a589ab444c4857c6ead0783a647068199490
Konboi <ryosuke.yabuki@gmail.com>
2013-04-26 12:05:38 +0900

    git-svnでcloneした際はsvm-modeが適用されるように変更

--- init/30_magit-mode.el
+++ init/30_magit-mode.el
@@ -5,4 +5,9 @@
 (global-set-key (kbd "C-c git") 'magit-status)
 (global-set-key (kbd "C-c dci") 'magit-svn-dcommit)
 
+(add-hook 'magit-mode-hook (lambda()
+                             (require 'magit-svn)
+                             (if (magit-svn-get-ref-info)
+                                 (magit-svn-mode))))
+
 

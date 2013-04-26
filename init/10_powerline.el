e6266d5abff6f802f424f1f907d88ba51b3a2a32
Konboi <ryosuke.yabuki@gmail.com>
2013-04-26 10:48:23 +0900

    Added powrline config

---
+++ init/10_powerline.el
@@ -0,0 +1,11 @@
+(require 'powerline)
+
+(setq powerline-color1 "#657b83")
+(setq powerline-color2 "#839496")
+
+(set-face-attribute 'mode-line nil
+                    :foreground "#fdf6e3"
+                    :background "##ff0066"
+                    :box nil)
+(set-face-attribute 'mode-line-inactive nil
+                    :box nil)

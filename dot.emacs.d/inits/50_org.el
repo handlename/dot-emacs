3e5835f92f2914c21ba73198e6b95ca033caf9ac
handlename <nagata@handlena.me>
2013-05-03 17:09:28 +0900

    update org-capture default header

--- dot.emacs.d/inits/50_org.el
+++ dot.emacs.d/inits/50_org.el
@@ -31,10 +31,10 @@
 
 ;; org-capture
 (defvar org-capture-templates
-      '(("T" "Todo private" entry (file+olp org-my-private-file "Todo") "** TODO %?\n   %U\n   %i\n" :unnarrowed t)
+      '(("T" "Todo private" entry (file+headline org-my-private-file "Todo") "** TODO %?\n   %U\n   %i\n" :unnarrowed t)
         ("I" "Idea private" entry (file+headline org-my-private-file "Idea") "** %?\n   %U\n   %i\n" :unnarrowed t)
         ("M" "Memo private" entry (file+headline org-my-private-file "Memo") "** %?\n   %U\n   %i\n" :unnarrowed t)
-        ("t" "Todo kayac" entry (file+olp org-my-kayac-file "Todo") "** TODO %?\n   %U\n   %i\n" :unnarrowed t)
+        ("t" "Todo kayac" entry (file+headline org-my-kayac-file "Todo") "** TODO %?\n   %U\n   %i\n" :unnarrowed t)
         ("i" "Idea kayac" entry (file+headline org-my-kayac-file "Idea") "** %?\n   %U\n   %i\n" :unnarrowed t)
         ("m" "Memo kayac" entry (file+headline org-my-kayac-file "Memo") "** %?\n   %U\n   %i\n" :unnarrowed t)
         ("d" "Do It Later" entry (file+headline org-my-dil-file "DIL") "** %?\n   %U\n   %i\n" :unnarrowed t)

496dc7a77f4e502e462ceeabf40ebe37941aec0a
handlename <nagata@handlena.me>
2013-05-03 17:13:05 +0900

    fix quickrun display nothing probrem

--- dot.emacs.d/inits/30_quickrun.el
+++ dot.emacs.d/inits/30_quickrun.el
@@ -1,2 +1,7 @@
 (require 'quickrun)
 (global-set-key (kbd "C-c e") 'quickrun)
+
+;; http://hyone.hatenablog.com/entry/20120127/1327680618
+(defadvice quickrun/apply-outputter
+  (after quickrun/fix-scroll-buffer activate)
+  (recenter))

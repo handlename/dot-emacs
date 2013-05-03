3a24cd9278a492a76bd83142b2ad6081d8d5371d
handlename <nagata@handlena.me>
2013-05-03 17:09:59 +0900

    update faces of magit

--- dot.emacs.d/inits/50_magit.el
+++ dot.emacs.d/inits/50_magit.el
@@ -1,6 +1,8 @@
 (require 'magit)
 
-(set-face-foreground 'magit-diff-add "#00ff00")
-(set-face-foreground 'magit-diff-del "#ff0000")
+(set-face-background 'magit-diff-file-header "#111122")
 (set-face-foreground 'magit-diff-hunk-header "#00ffff")
-(set-face-background 'magit-item-highlight nil)
+(set-face-background 'magit-diff-hunk-header "#111122")
+(set-face-background 'magit-item-highlight   "#111122")
+(set-face-foreground 'magit-diff-add         "#00ff00")
+(set-face-foreground 'magit-diff-del         "#ff0000")

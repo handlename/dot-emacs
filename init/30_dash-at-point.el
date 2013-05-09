f1b23b697e4b4c5d98e5f6653a806c9895bb22df
Konboi <ryosuke.yabuki@gmail.com>
2013-05-09 11:34:34 +0900

    コマンドが競合するので修正

--- init/30_dash-at-point.el
+++ init/30_dash-at-point.el
@@ -1,3 +1,3 @@
 (autoload 'dash-at-point "dash-at-point"
           "Search the word at point with Dash." t nil)
-(global-set-key "\C-cd" 'dash-at-point)
+(global-set-key "\C-c \C-d" 'dash-at-point)

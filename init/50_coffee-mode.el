0f8d3ddc035a98740959b4907a662d785910cedb
Konboi <ryosuke.yabuki@gmail.com>
2013-05-16 16:57:34 +0900

    インデント修正

--- init/50_coffee-mode.el
+++ init/50_coffee-mode.el
@@ -4,3 +4,14 @@
 ;; flymake-coffee
 (when (require 'flymake-coffee nil t)
   (add-hook 'coffee-mode-hook 'flymake-coffee-load))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2)
+ (setq coffee-tab-width 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
+
+(add-to-list 'auto-mode-alist '("\\.coffee" . coffee-mode))

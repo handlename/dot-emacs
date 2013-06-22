4282fe8d0efadbd818436f547361f4e7f6fc8745
Konboi <ryosuke.yabuki@gmail.com>
2013-06-22 10:46:13 +0900

    Added emacs-git-messenger

--- init/30_emacs-git-messenger.el
+++ init/30_emacs-git-messenger.el
@@ -1,7 +1,6 @@
 (require 'git-messenger)
 
 ;; always show commit message
-(setq git-messenger:show-detail 't)
-
+(setq git-messenger:show-detail t)
 (global-set-key (kbd "C-x g m") 'git-messenger:popup-message)
 

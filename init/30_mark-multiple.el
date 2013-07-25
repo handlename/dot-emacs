ae9e6867d98d0f921c8efdba06bba1465370dc59
Konboi <ryosuke.yabuki@gmail.com>
2013-07-25 12:39:38 +0900

    修正

--- init/30_mark-multiple.el
+++ init/30_mark-multiple.el
@@ -6,8 +6,7 @@
 
 (global-set-key (kbd "C-,") 'mark-previous-like-this)
 (global-set-key (kbd "C-.") 'mark-next-like-this)
-;; (global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
-(global-set-key (kbd "C-8") 'mark-all-like-this)
+;; kglobaley (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)global-kglobaley (kbd "C-8") 'mark-all-like-this)
 
 ;; (add-hook 'sgml-mode-hook
 ;;           (lambda ()

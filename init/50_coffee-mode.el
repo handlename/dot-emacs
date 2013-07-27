5e0013190a73efd7f78cffce6ed097e899d8b09a
Konboi <ryosuke.yabuki@gmail.com>
2013-07-27 15:06:30 +0900

    Added flymake-coffee

--- init/50_coffee-mode.el
+++ init/50_coffee-mode.el
@@ -1,17 +1,14 @@
 ;; coffee-mode
 (require 'coffee-mode)
-
-;; flymake-coffee
-(when (require 'flymake-coffee nil t)
-  (add-hook 'coffee-mode-hook 'flymake-coffee-load))
+(require 'flymake-coffee)
+(add-hook 'coffee-mode-hook 'flymake-coffee-load)
 
 (defun coffee-custom ()
-  "coffee-mode-hook"
- (set (make-local-variable 'tab-width) 2)
- (setq coffee-tab-width 2))
-
+ "coffee-mode-hook"
+  (and (set (make-local-variable 'tab-width) 2)
+       (set (make-local-variable 'coffee-tab-width) 2))
+  )
 (add-hook 'coffee-mode-hook
   '(lambda() (coffee-custom)))
-
 
 (add-to-list 'auto-mode-alist '("\\.coffee" . coffee-mode))

4258a5fa25761f92a82c06aad23a22d9f461a825
Konboi <ryosuke.yabuki@gmail.com>
2013-08-07 11:36:31 +0900

    helmに移行

--- init/50_helm-rails.el
+++ init/50_helm-rails.el
@@ -1,6 +1,6 @@
 (require 'helm-rails)
 
-(define-key global-map (kbd "s-t") 'helm-rails-controllers)
+(define-key global-map (kbd "C-c C-c") 'helm-rails-controllers)
 (define-key global-map (kbd "s-y") 'helm-rails-models)
 (define-key global-map (kbd "s-u") 'helm-rails-views)
 (define-key global-map (kbd "s-o") 'helm-rails-specs)

e9655289d4c9607021e6ebed6d2d319942117198
Konboi <ryosuke.yabuki@gmail.com>
2013-09-30 15:16:35 +0900

    Added markdown type

--- init/30_markdown-mode.el
+++ init/30_markdown-mode.el
@@ -1,3 +1,3 @@
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (setq auto-mode-alist (cons '("\\.md" . markdown-mode) auto-mode-alist))
-
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))

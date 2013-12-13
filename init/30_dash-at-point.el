7e9891437b1bb9a5b0bf478fbd8fbbc39ea6298b
Konboi <ryosuke.yabuki@gmail.com>
2013-12-13 23:50:49 +0900

    コマンド変更

--- init/30_dash-at-point.el
+++ init/30_dash-at-point.el
@@ -1,3 +1,3 @@
 (autoload 'dash-at-point "dash-at-point"
           "Search the word at point with Dash." t nil)
-(global-set-key (kbd "C-c C-d") 'dash-at-point)
+(global-set-key (kbd "C-c M-d") 'dash-at-point)

ac6c3a067c3f870c7b82a30c312225600d90e194
Konboi <ryosuke.yabuki@gmail.com>
2014-05-31 12:10:57 +0900

    update global key

--- init/30_helm.el
+++ init/30_helm.el
@@ -6,7 +6,6 @@
 
 ;; key bind
 (global-set-key (kbd "C-c C-g") 'helm-ag)
-(define-key global-map (kbd "C-s")   'helm-c-moccur-occur-by-moccur)
 (define-key global-map (kbd "C-x b") 'helm-for-files)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-c C-i") 'helm-imenu)

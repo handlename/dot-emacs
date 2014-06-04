34f0e2e4376dd56fd6fcd42071c23820e04f8fcb
Konboi <ryosuke.yabuki@gmail.com>
2014-06-04 23:28:49 +0900

    Added magit config

--- inits/30_magit-mode.el
+++ inits/30_magit-mode.el
@@ -3,13 +3,7 @@
 (push '(magit-status :position left :height 0.5) popwin:special-display-config)
 
 (global-set-key (kbd "C-c git") 'magit-status)
-(global-set-key (kbd "C-c dci") 'magit-svn-dcommit)
-
-(add-hook 'magit-mode-hook (lambda()
-                             (require 'magit-svn)
-                             (if (magit-svn-get-ref-info)
-                                 (magit-svn-mode))))
-
+(global-set-key (kbd "C-c pu")  'magit-push)
 
 (eval-after-load 'magit
   '(progn

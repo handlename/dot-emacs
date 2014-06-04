0a2e2f878ce833361875c466f1ec8f3fc0bb8266
Konboi <ryosuke.yabuki@gmail.com>
2014-06-04 23:29:17 +0900

    Added file type for ruby-mode

--- inits/50_ruby-mode.el
+++ inits/50_ruby-mode.el
@@ -41,4 +41,5 @@
 (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.cap$" . ruby-mode))

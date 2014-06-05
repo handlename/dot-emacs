bdf4159bd3b203d1908456fd41260956e305a777
Konboi <ryosuke.yabuki@gmail.com>
2014-06-05 22:02:29 +0900

    Added tx-mode

--- inits/50_tx-mode.el
+++ inits/50_tx-mode.el
@@ -1,9 +1,2 @@
-(autoload 'tx-mode "tx-mode"
-         "Major mode for editing Text::Xslate syntax")
+(require 'tx-mode)
 (add-to-list 'auto-mode-alist '("\\.tx$" . tx-mode))
-
-(add-hook 'tx-mode-hook
-          '(lambda ()
-             (setq tx-tag-start-char "[%")
-             (setq tx-tag-end-char "%]")
-             (setq tx-line-start-char "%")))

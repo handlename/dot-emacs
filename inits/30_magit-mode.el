a579cbc92c62a943108b3ef328f394997afe29ec
Konboi <ryosuke.yabuki@gmail.com>
2014-06-05 22:02:04 +0900

    Change key-prefix

--- inits/30_magit-mode.el
+++ inits/30_magit-mode.el
@@ -3,7 +3,7 @@
 (push '(magit-status :position left :height 0.5) popwin:special-display-config)
 
 (global-set-key (kbd "C-c git") 'magit-status)
-(global-set-key (kbd "C-x pu")  'magit-push)
+(global-set-key (kbd "C-x C-p")  'magit-push)
 
 (eval-after-load 'magit
   '(progn

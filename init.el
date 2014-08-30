fb55ac1a720244ce22aadc71badde05353e362d3
handlename <nagata@handlena.me>
2014-08-30 14:41:09 +0900

    basic

--- init.el
+++ init.el
@@ -12,6 +12,31 @@
   (unless (package-installed-p package)
     (package-install package)))
 
+;;;; init-loader
+(require 'init-loader)
+
+;;; load except compiled file.
+;;; http://d.hatena.ne.jp/sugyan/20120101/1325429637
+(eval-when-compile (require 'cl))
+(flet ((init-loader--re-load-files
+    (re dir &optional sort)
+    (loop for el in (directory-files dir t)
+          when (and (string-match re (file-name-nondirectory el))
+            (or (string-match "elc$" el)
+                (and (string-match "el$" el)
+                 (not (locate-library (concat el "c"))))))
+          collect (file-name-nondirectory el) into ret
+          finally return (if sort (sort ret 'string<) ret))))
+  (init-loader-load (concat user-emacs-directory "/inits")))
+
+;; inits/* filenome prefix:
+;;   - 00 : fundamental settings
+;;   - 10 : pre load settings
+;;   - 20 : custom functions
+;;   - 30 : minor mode settings
+;;   - 40 : major mode settings
+;;   - 90 : post load settings
+
 ;;;; key
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (setq ns-command-modifier (quote meta))

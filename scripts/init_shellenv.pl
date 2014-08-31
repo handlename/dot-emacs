2ba23e8be9648df8f508c4afb02baa2a2b7b4b30
handlename <nagata@handlena.me>
2014-08-31 09:52:00 +0900

    sort shellenv.el

--- scripts/init_shellenv.pl
+++ scripts/init_shellenv.pl
@@ -5,7 +5,7 @@ use warnings;
 my $out = "$ENV{HOME}/.emacs.d/shellenv.el";
 open my $fh, '>', $out or die "failed to open file: ${out}";
 
-for my $name (keys %ENV) {
+for my $name (sort keys %ENV) {
     next if ! exists $ENV{$name};
     printf $fh qq{(setenv "%s" "%s")\n}, $name, $ENV{$name};
 }

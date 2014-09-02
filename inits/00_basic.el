2e86c6b49396641f9c083df4afd1d41df1684d1d
handlename <nagata@handlena.me>
2014-09-02 12:16:11 +0900

    more basic settings

--- inits/00_basic.el
+++ inits/00_basic.el
@@ -42,3 +42,33 @@
 
 ;;; for m-x shell
 (setq explicit-shell-file-name "/usr/local/bin/zsh")
+
+;;; encoding settings
+(set-language-environment      "Japanese")
+(prefer-coding-system          'utf-8-unix)
+(setq                          buffer-file-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(set-terminal-coding-system    'utf-8)
+(set-keyboard-coding-system    'utf-8)
+(set-clipboard-coding-system   'utf-8)
+
+;;; decrease gc
+(setq gc-cons-threshold (* 50 gc-cons-threshold))
+
+;;; log settings
+(setq message-log-max 10000)
+(setq history-length 1000)
+
+;;; dialogue
+(setq use-dialog-box nil)
+(defalias 'message-box 'message)
+
+;;; make y/n alias of yes/no
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;;; stop blink cursor
+(blink-cursor-mode 0)
+
+;;; upcase and downcase
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)

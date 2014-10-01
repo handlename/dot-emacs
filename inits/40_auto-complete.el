f2fd43471fca938ac8d3989e32dd9505af83e85c
handlename <nagata@handlena.me>
2014-10-01 11:50:35 +0900

    enable auto-complete for sh-mode

--- inits/40_auto-complete.el
+++ inits/40_auto-complete.el
@@ -8,9 +8,11 @@
                  cperl-mode
                  emacs-lisp-mode
                  go-mode
+                 sh-mode
                  ))
 
 ;;; keybindings
 (define-key ac-completing-map (kbd "M-n") 'ac-next)
 (define-key ac-completing-map (kbd "M-p") 'ac-previous)
 (define-key ac-completing-map (kbd "M-s") 'ac-isearch)
+(define-key ac-completing-map (kbd "C-m") 'newline-and-indent)

e61727179d7158168f3996f2f2bbe268179d316e
handlename <nagata@handlena.me>
2014-10-01 12:19:26 +0900

    init-loader is not load raw file if there are byte compiled file. see https://github.com/emacs-jp/init-loader/blob/master/init-loader.el#L227

--- init.el
+++ init.el
@@ -45,16 +45,4 @@
 
 ;;;; init-loader
 (require 'init-loader)
-
-;;; load except compiled file.
-;;; http://d.hatena.ne.jp/sugyan/20120101/1325429637
-(flet ((init-loader--re-load-files
-    (re dir &optional sort)
-    (loop for el in (directory-files dir t)
-          when (and (string-match re (file-name-nondirectory el))
-            (or (string-match "elc$" el)
-                (and (string-match "el$" el)
-                 (not (locate-library (concat el "c"))))))
-          collect (file-name-nondirectory el) into ret
-          finally return (if sort (sort ret 'string<) ret))))
-  (init-loader-load (concat user-emacs-directory "/inits")))
+(init-loader-load (concat user-emacs-directory "/inits"))

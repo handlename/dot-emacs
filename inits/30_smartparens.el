c31608a65e6ac201cb7a902d1adf80cf87f10427
handlename <nagata@handlena.me>
2014-12-02 23:37:19 +0900

    add web-mode

--- inits/30_smartparens.el
+++ inits/30_smartparens.el
@@ -1,2 +1,5 @@
 (require 'smartparens-config)
 (smartparens-global-mode t)
+
+(sp-local-pair 'web-mode "<: " " :>" :trigger "::")
+(sp-local-pair 'web-mode "[% " " %]" :trigger "%%")
---
+++ inits/40_web.el
@@ -0,0 +1,3 @@
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tx$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tt$" . web-mode))

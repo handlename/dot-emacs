ceb71545725cd4646dcb3983fdec75c3e536326e
handlename <nagata@handlena.me>
2015-03-11 16:24:20 +0900

    fit configuration for new version of popwin

--- inits/30_popwin.el
+++ inits/30_popwin.el
@@ -1,21 +1,22 @@
 (require 'popwin)
+(popwin-mode 1)
 
-(setq special-display-function 'popwin:special-display-popup-window)
-(setq special-display-regexps '(
-                                "\\*BackTrace\\*"
-                                "\\*Compile-Log\\*"
-                                "\\*ENSIME-"
-                                "\\*Inspector\\*"
-                                "\\*Help\\*"
-                                "\\*auto-async-byte-compile\\*"
-                                "\\*blockdiag\\*"
-                                "\\*compilation\\*"
-                                "\\*Gofmt Errors\\*"
-                                "\\*godoc"
-                                "\\*helm"
-                                "\\*quickrun\\*"
-                                "\\.po"
-                                ))
+(setq popwin:special-display-config
+      '(
+        ("^\\*BackTrace\\*$" :regexp t)
+        ("^\\*Compile-Log\\*$" :regexp t)
+        ("^\\*ENSIME-" :regexp t)
+        ("^\\*Inspector\\*$" :regexp t)
+        ("^\\*Help\\*$" :regexp t)
+        ("^\\*auto-async-byte-compile\\*$" :regexp t)
+        ("^\\*blockdiag\\*$" :regexp t)
+        ("^\\*compilation\\*$" :regexp t)
+        ("^\\*Gofmt Errors\\*$" :regexp t)
+        ("^\\*godoc" :regexp t)
+        ("^\\*[hH]elm" :regexp t)
+        ("^\\*quickrun\\*$" :regexp t)
+        ("\\.po$" :regexp t)
+        ))
 
 (setq popwin:popup-window-height 0.33)
 (setq popwin:popup-window-width  0.33)

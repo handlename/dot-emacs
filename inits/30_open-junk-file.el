eda2ff249488a6a401bc418ca357bddf2891b31e
handlename <nagata@handlena.me>
2015-05-08 16:05:10 +0900

    prefix for functions defined in local is `my:`

--- inits/30_open-junk-file.el
+++ inits/30_open-junk-file.el
@@ -1,15 +1,15 @@
 (require 'open-junk-file)
-(defvar junk-file-dir "~/junk")
-(defvar junk-file-format-base (concat junk-file-dir "/%Y/%m/%Y%m%d_%H%M%S"))
-(setq open-junk-file-format (concat junk-file-format-base "."))
+(defvar my:junk-file-dir "~/junk")
+(defvar my:junk-file-format-base (concat my:junk-file-dir "/%Y/%m/%Y%m%d_%H%M%S"))
+(setq open-junk-file-format (concat my:junk-file-format-base "."))
 (setq open-junk-file-find-file-function 'find-file)
 
 ;;; open-junk-dir
-(defvar my:open-junk-dir-format (concat junk-file-format-base "_"))
+(defvar my:open-junk-dir-format (concat my:junk-file-format-base "_"))
 
 (defun my:open-junk-dir ()
   (interactive)
-  (let* ((dir-base (format-time-string open-junk-dir-format (current-time)))
+  (let* ((dir-base (format-time-string my:open-junk-dir-format (current-time)))
          (dir (read-string "Dirname: " dir-base)))
     (make-directory dir t)
     (find-file dir)))
@@ -22,24 +22,24 @@
 (defvar my:junk-file-list
   (reverse
    (eshell-extended-glob (concat
-                          (file-name-as-directory junk-file-dir)
+                          (file-name-as-directory my:junk-file-dir)
                           "*/*/????????_??????*"))))
 
 (defvar my:helm-source-junk-file
       '((name . "Junk files")
-        (candidates . junk-file-list)
+        (candidates . my:junk-file-list)
         (type . file)))
 
 (defun my:helm-junk-file ()
   (interactive)
-  (helm :sources '(helm-source-junk-file)))
+  (helm :sources '(my:helm-source-junk-file)))
 
 ;;; ag-junk-file
 (require 'ag)
 
 (defun my:ag-junk-file (query)
   (interactive "sSearch string: ")
-  (ag query junk-file-dir))
+  (ag query my:junk-file-dir))
 
 ;;; key bindings
 (define-prefix-command 'my:junk-keymap)

062ca65fb447087d2193d1d63be05177d7c0cf34
handlename <nagata@handlena.me>
2015-11-30 17:28:30 +0900

    change key binding for replace-regexp
    C-M-r is overlapped by DisplayMaid.app

--- inits/01_keybidings.el
+++ inits/01_keybidings.el
@@ -9,8 +9,8 @@
 (global-set-key (kbd "C-h")     'backward-delete-char)
 (global-set-key (kbd "C-x C--") 'goto-line)
 (global-set-key (kbd "C-t")     'next-multiframe-window)
-(global-set-key (kbd "C-M-r")   'replace-regexp)
 (global-set-key (kbd "C-r")     'replace-string)
+(global-set-key (kbd "C-S-r")   'replace-regexp)
 
 ;;; disable C-x C-b to avoid miss hit of C-x b...
 (global-set-key (kbd "C-x C-b") nil)
